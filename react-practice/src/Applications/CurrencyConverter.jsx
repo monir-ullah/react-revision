6ea7724177749489996d6041eb158ddc81fba978
Monir Ullah <mullah725@gmail.com>
2024-01-05 18:58:46 +0600

    currency completed

--- react-practice/src/Applications/CurrencyConverter.jsx
+++ react-practice/src/Applications/CurrencyConverter.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import InputBox from "../components/InputBox";
+import useCurrencyInfo from "../hooks/useCurrencyInfo";
 
 function CurrencyConverter() {
+  const [amount, setAmount] = useState(0);
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("bdt");
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const currencyOptions = Object.keys(currencyInfo);
+
   return (
     <div
-      className="w-screen grid h-screen justify-center items-center opacity-90 "
+      className="w-screen grid h-screen justify-center items-center opacity-90 py-14"
       style={{
         backgroundImage: `url("https://cdn.pixabay.com/photo/2017/07/30/10/00/economy-2553884_1280.jpg")`,
         backgroundRepeat: "no-repeat",
@@ -15,7 +26,46 @@ function CurrencyConverter() {
           Currency Converter <span>(Project 3)</span>
         </h1>
 
-        <InputBox />
+        <form
+          className="flex flex-col items-center"
+          onSubmit={(e) => {
+            e.preventDefault();
+            setConvertedAmount((amount * currencyInfo[to]).toFixed(2));
+          }}
+        >
+          <InputBox
+            label="From"
+            amount={amount}
+            currencyOptions={currencyOptions}
+            onAmountChange={(amount) => setAmount(amount)}
+            onCurrencyChange={(currency) => setFrom(currency)}
+            selectedCurrency={from}
+          />
+          {/*  Swap Button  . Here I'm implementing the currency swap function with onClick handler */}
+
+          <button
+            onClick={() => {
+              setFrom(to);
+              setTo(from);
+              setAmount(convertedAmount);
+              setConvertedAmount(amount);
+            }}
+            className="bg-blue-600 w-1/4 hover:bg-blue-800 -mb-3 py-2 px-5 text-slate-100  rounded-md relative shadow-lg"
+          >
+            Swap
+          </button>
+          <InputBox
+            label="To"
+            amount={convertedAmount}
+            currencyOptions={currencyOptions}
+            onAmountChange={(amount) => setAmount(amount)}
+            onCurrencyChange={(currency) => setTo(currency)}
+            selectedCurrency={to}
+          />
+          <button className="w-full bg-blue-600 rounded-md hover:bg-blue-800 py-3 text-slate-100 shadow-lg my-5">
+            Convert Currency {from.toUpperCase()} to {to.toUpperCase()}
+          </button>
+        </form>
       </div>
     </div>
   );
--- react-practice/src/components/InputBox.jsx
+++ react-practice/src/components/InputBox.jsx
@@ -1,119 +1,52 @@
-import { useState } from "react";
-import useCurrencyInfo from "../hooks/useCurrencyInfo";
-
-function InputBox() {
-  const [amount, setAmount] = useState(0);
-  const [convertedAmount, setConvertedAmount] = useState(0);
-  const [from, setFrom] = useState("usd");
-  const [to, setTo] = useState("bdt");
-
-  const currencyInfo = useCurrencyInfo(from);
-
-  const options = Object.keys(currencyInfo);
-
+/* eslint-disable react/prop-types */
+function InputBox({
+  label,
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  selectedCurrency,
+  currencyOptions = [],
+  // amountDisable = false,
+}) {
   return (
-    <form
-      className="flex flex-col items-center"
-      onSubmit={(e) => {
-        e.preventDefault();
-        setConvertedAmount((amount * currencyInfo[to]).toFixed(2));
-      }}
-    >
-      <div className="w-full flex gap-6 bg-slate-100 py-5 px-5 rounded -mb-3">
-        <div className="w-1/2">
-          <label htmlFor="currencyFrom" className="w-full">
-            From
-          </label>
-          <input
-            type="number"
-            value={amount}
-            min={0}
-            step={0.01}
-            onChange={(e) => setAmount(e.target.value)}
-            name="currencyFrom"
-            id="currencyFrom"
-            className="w-full py-2 px-5 rounded my-2"
-          />
-        </div>
-        <div className="w-1/2 text-end">
-          <label htmlFor="currencyType" className="w-full text-center">
-            Currency Type
-          </label>
-          <select
-            name=""
-            id="currencyType"
-            value={from}
-            className="w-full py-2 px-5 rounded my-2"
-            onChange={(e) => {
-              setFrom(e.target.value);
-            }}
-          >
-            {options.map((currency) => (
-              <option key={currency} value={currency}>
-                {currency.toUpperCase()}
-              </option>
-            ))}
-          </select>
-        </div>
+    <div className="w-full flex gap-6 bg-slate-100 py-5 px-5 rounded -mb-3">
+      <div className="w-1/2">
+        <label htmlFor="currencyFrom" className="w-full">
+          {label}
+        </label>
+        <input
+          type="number"
+          value={amount}
+          min={0}
+          step={0.01}
+          onChange={(e) => onAmountChange && onAmountChange(e.target.value)}
+          // disabled={amountDisable}
+          name="currencyFrom"
+          id="currencyFrom"
+          className="w-full py-2 px-5 rounded my-2"
+        />
       </div>
-
-      {/*  Swap Button  */}
-
-      <button
-        onClick={() => {
-          setFrom(to);
-          setTo(from);
-          setAmount(convertedAmount);
-          setConvertedAmount(amount);
-        }}
-        className="bg-blue-600 w-1/4 hover:bg-blue-800 py-2 px-5 text-slate-100  rounded-md relative shadow-lg"
-      >
-        Swap
-      </button>
-
-      <div className="w-full flex gap-6 bg-slate-100 py-5 px-5 rounded -mt-3 ">
-        <div className="w-1/2">
-          <label htmlFor="currencyFrom" className="w-full">
-            To
-          </label>
-          <input
-            type="number"
-            min={0}
-            readOnly
-            value={convertedAmount}
-            onChange={(e) => {
-              setTo(e.target.value);
-            }}
-            name="currencyFrom"
-            id="currencyFrom"
-            className="w-full py-2 px-5 rounded my-2"
-          />
-        </div>
-        <div className="w-1/2 text-end">
-          <label htmlFor="currencyType" className="w-full text-center">
-            Currency Type
-          </label>
-          <select
-            name=""
-            id="currencyType"
-            value={to}
-            className="w-full py-2 px-5 rounded my-2"
-            onChange={(e) => {
-              setTo(e.target.value);
-            }}
-          >
-            {options.map((currency) => (
-              <option key={currency} value={currency}>
-                {currency.toUpperCase()}
-              </option>
-            ))}
-          </select>
-        </div>
+      <div className="w-1/2 text-end">
+        <label htmlFor="currencyType" className="w-full text-center">
+          Currency Type
+        </label>
+        <select
+          name=""
+          id="currencyType"
+          value={selectedCurrency}
+          className="w-full py-2 px-5 rounded my-2"
+          onChange={(e) => {
+            onCurrencyChange && onCurrencyChange(e.target.value);
+          }}
+        >
+          {currencyOptions.map((currency) => (
+            <option key={currency} value={currency}>
+              {currency.toUpperCase()}
+            </option>
+          ))}
+        </select>
       </div>
-      <button className="w-full bg-blue-600 rounded-md hover:bg-blue-800 py-3 text-slate-100 shadow-lg my-5">
-        Convert Currency {from.toUpperCase()} to {to.toUpperCase()}
-      </button>
-    </form>
+    </div>
   );
 }
 
